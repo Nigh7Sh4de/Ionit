b198b095e69849fdf1c88eb9039091ebd5dffb44
nigh7sh4de <plotnikd@gmail.com>
2017-10-25 13:50:44 -0400

    Add methods to fetch/update data

--- js/App.js
+++ js/App.js
@@ -4,27 +4,60 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
+  AsyncStorage,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
-import FilterView from './FilterView';
-import DataView from './DataView';
-import ActionsView from './ActionsView';
+import FilterView from './FilterView'
+import DataView from './DataView'
+import ActionsView from './ActionsView'
 
-import Styles from './Styles';
+import Styles from './Styles'
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  
+  fetchData() {
+    AsyncStorage.getItem('data')
+    .then((data) => {
+      console.log(data)
+      this.setState({
+        data
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  saveData() {
+    AsynStorage.setItem('data', this.state.data.toString())
+    .then((result) => {
+      console.log('Saved', result)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  componentWillMount() {
+      this.fetchData()
+  }
+
   render() {
     return (
       <View>
         <FilterView style={Styles} />
-        <DataView />
+        <DataView data={this.state.data} />
         <ActionsView />
       </View>
-    );
+    )
   }
 }
