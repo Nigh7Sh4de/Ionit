3fd2e99f4a372b18b6e6e99964115d9fd474d177
nigh7sh4de <plotnikd@gmail.com>
2017-12-28 15:18:36 -0500

    Move event detail view to inline expanded DataView row

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import {
 
 import LoginView from '~/views/LoginView'
 import DataView from '~/views/DataView'
-import EventDetailView from '~/views/EventDetailView'
 import EditEventView from '~/views/EditEventView'
 
 import Styles from './Styles'
@@ -25,7 +24,7 @@ import Styles from './Styles'
 const Scenes = Actions.create(
   <Stack key='root'>
     <Scene key='data' component={DataView} title='Data' />
-    <Scene key='event' path='/event/:id' component={EventDetailView} title='Event' />
+    <Scene key='event' path='/event/:id' component={DataView} title='Data' />
     <Scene key='newEvent' path='/event/new' component={EditEventView} title='New Event' />
     <Scene key='editEvent' path='/event/:id/edit' component={EditEventView} title='Edit Event' />
     <Scene key='newChildEvent' path='/event/:id/newchild' component={EditEventView} title='New Child Event' />
